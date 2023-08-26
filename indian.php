c6fc26444f9a6395857ccc8d1fecc86e77cc1b7f
De122 <101280206+De122@users.noreply.github.com>
2023-08-27 02:10:04 +0530

    Update indian.php

--- indian.php
+++ indian.php
@@ -20,25 +20,28 @@ $result = $conn->query($sql);
 <!DOCTYPE html>
 <html>
 <head>
-
+<link href="/static/disheslist.css" rel="stylesheet"></link>
 </head>
 <body>
-    <h2>Snack Recipes</h2>
-    <ul>
+    <div class="container">
+    <h1>Indian Recipes</h2>
+    <ol>
     <?php
     if ($result->num_rows > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $itemId = $row['id'];
             $itemName = $row['name'];
+            echo "<li>";
             echo "<a href='item_details.php?item_id=$itemId'>$itemName</a><br>";
+            echo "</li>";
         }
         
     } else {
         echo "No data found";
     }
     ?>
-    </ul>
-    
+    </ol>
+</div>
     <?php
     // Step 4: Close the database connection
     $conn->close();
